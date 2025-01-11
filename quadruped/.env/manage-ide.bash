5fe1cf459122c97b4cf5954769aa5809e0c6a94d
trantor <postrantor@gmail.com>
2025-01-11 20:35:39 +0800

    [vscode] add vscode recommendations extensions
    Signed-off-by: trantor <postrantor@gmail.com>

--- quadruped/.env/manage-ide.bash
+++ quadruped/.env/manage-ide.bash
@@ -5,5 +5,17 @@
 # ---
 
 function import-vscode-extionsions() {
-  cp -r --archive ${QUAD_WORKDIR}/.vscode/extensions/ ${HOME}/.vscode-server/extensions/*
+  # 遍历 ${QUAD_WORKDIR}/.vscode/extensions/ 下的每个文件夹
+  find "${QUAD_WORKDIR}/.vscode/extensions/" -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
+    # 复制文件夹
+    cp -r --archive "$dir" "${HOME}/.vscode-server/extensions/"
+    # 输出一个 . 符号
+    echo -n "."
+  done
+  # 输出换行符，避免后续输出与 . 符号在同一行
+  echo
+
+  cp "${QUAD_WORKDIR}/.vscode/extensions/extensions.json" --archive "${HOME}/.vscode-server/extensions/extensions.json"
+
+  rm -rf "${QUAD_WORKDIR}/.vscode/extensions
 }
