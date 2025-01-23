6cb734bcd2d30d0256c986a895ec39511c79f0c8
trantor <trantor@gmail.com>
2025-01-23 19:03:02 +0800

    [quad] add create-user.bash
    Signed-off-by: trantor <trantor@gmail.com>

--- quadruped/.env/create-user.bash
+++ quadruped/.env/create-user.bash
@@ -1,13 +1,8 @@
 #! /bin/bash
-# create-user.bash trantor 1000
 
 USERNAME=$1
-USER_UID=$2
-USER_GID=$USER_UID
 
-groupadd --gid $USER_GID $USERNAME
-useradd -s /bin/bash --uid $USER_UID --gid $USER_GID -m $USERNAME
+adduser ${USERNAME} --force-badname
+usermod -aG sudo ${USERNAME}
 
-apt update && apt install -y sudo
-echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME
-chmod 0440 /etc/sudoers.d/$USERNAME
+su ${USERNAME}
