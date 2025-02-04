e3d7f7f70d76d2cbf69cf2f9ada6e8db41bddb11
postrantor <postrantor@gmail.com>
2025-02-04 23:28:39 +0800

    [quad] add matlab repos
    Signed-off-by: postrantor <postrantor@gmail.com>

--- quadruped/.env/manage-project.bash
+++ quadruped/.env/manage-project.bash
@@ -292,7 +292,7 @@ function ignore-colcon-pkg() {
     [reference]="reference"
     [interfaces]="interfaces/geometry2"
     [simulation]="simulation"
-    [matlab]="quadruped/base-matlab-mpc"
+    [matlab]="quadruped/matlab"
   )
 
   # Iterate over the ignore list
