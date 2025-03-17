83c8bd652066d2423dd6e25d5a8ca935ae5fe91a
postrantor <postrantor@gmail.com>
2025-03-17 14:32:09 +0800

    [red] add plantform dependencies in bash script
    Signed-off-by: postrantor <postrantor@gmail.com>

--- red/.env/install-ros2-dependnce.bash
+++ red/.env/install-ros2-dependnce.bash
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 if [ -d /opt/ros/humble ]; then
   echo "ROS 2 Humble is already installed"
@@ -6,14 +6,14 @@ if [ -d /opt/ros/humble ]; then
 fi
 
 # https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debs.html#set-locale
-locale  # check for UTF-8
+locale # check for UTF-8
 
 sudo apt update && sudo apt install locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-locale  # verify settings
+locale # verify settings
 
 # https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debs.html#setup-sources
 sudo apt install software-properties-common
@@ -22,7 +22,7 @@ sudo add-apt-repository universe
 sudo apt update && sudo apt install curl -y
 sudo -E curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list >/dev/null
 
 # https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debs.html#install-ros-2-packages
 sudo apt update
@@ -46,3 +46,8 @@ sudo apt install -y \
   python3-flake8-quotes \
   python3-pytest-repeat \
   python3-pytest-rerunfailures
+
+# for af plantform
+sudo apt install -y \
+  bison flex \
+  libprotobuf-dev protobuf-compiler uuid-dev libsqlite3-dev libglib2.0-dev libglibmm-2.4-dev dbus-x11 python-importlib libfmt-dev
